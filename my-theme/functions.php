f3f54a96c64517180b25a140a5eb5e9c0d81e39f
nguyenviethoang1411 <axitray1234@gmail.com>
2021-05-25 04:42:42 +0700

    finalized theme

--- my-theme/functions.php
+++ my-theme/functions.php
@@ -1,9 +1,89 @@
 <?php
+
+function team06_theme_support(){
+
+
+    // Adds dynamic title tags
+    add_theme_support('title-tag');
+
+    add_theme_support('custom-logo');
+
+    add_theme_support('post-thumbnails');
+
+}
+
+add_action('after_setup_theme','team06_theme_support');
+
+
+
+function team06_menus(){
+
+    $locations = array(
+
+        'primary' => "Desktop Primary Left Sidebar",
+        'footer' => "Footer Menu Items"
+
+    );
+    register_nav_menus($locations);
+
+}
+
+add_action('init','team06_menus');
+
 function team06_register_styles(){
 
-    wp_enqueue_style('style' , get_stylesheet_uri()); 
+    $version = wp_get_theme()->get( 'Version' );
+
+    wp_enqueue_style('style' , get_stylesheet_uri(), array('team06-bootstrap'), $version, 'all'); 
+    wp_enqueue_style('team06-bootstrap' , "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all' ); 
+    wp_enqueue_style('team06-fontawesome' , "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all' ); 
 }
 
 add_action( 'wp_enqueue_scripts', 'team06_register_styles');
+
+
+function team06_register_scripts(){
+
+    wp_enqueue_script('team06-jquery','https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
+    wp_enqueue_script('team06-popper','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
+    wp_enqueue_script('team06-bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '3.4.1', true);
+    wp_enqueue_script('team06-main', get_template_directory_uri()."/assets/js/main.js", array(), '1.0', true);
+
+
+}
+
+add_action( 'wp_enqueue_scripts', 'team06_register_scripts');
+
+
+function team06_widget_areas(){
+
+    register_sidebar(
+        array(
+            'before_title' => '',
+            'after_title' => '',
+            'before_widget' => '',
+            'after_widget' => '',
+            'name' => 'Sidebar Area',
+            'id' => 'sidebar-1',
+            'description' => 'Sidebar Widget Area'
+ 
+        )
+    );
+
+    register_sidebar(
+        array(
+            'before_title' => '',
+            'after_title' => '',
+            'before_widget' => '',
+            'after_widget' => '',
+            'name' => 'Footer Area',
+            'id' => 'footer-1',
+            'description' => 'Footer Widget Area'
+ 
+        )
+    );
+}
+
+add_action( 'widgets_init', 'team06_widget_areas');
 
 ?>
